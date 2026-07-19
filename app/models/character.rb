a592673dd0614cad5eb055c5a75590302fa9eb07
Almeida.Levi <almeidalevi878@gmail.com>
2026-07-19 20:55:38 -0300

    add origin enum to character model

--- app/models/character.rb
+++ app/models/character.rb
@@ -8,4 +8,33 @@ class Character < ApplicationRecord
       5
     end
   end
+
+  enum :origin, {
+    academico: "academico",
+    agente_de_saude: "agente_de_saude",
+    amnesico: "amnesico",
+    artista: "artista",
+    atleta: "atleta",
+    chef: "chef",
+    criminoso: "criminoso",
+    cultista_arrependido: "cultista_arrependido",
+    desgarrado: "desgarrado",
+    engenheiro: "engenheiro",
+    executivo: "executivo",
+    investigador: "investigador",
+    lutador: "lutador",
+    magnata: "magnata",
+    mercenario: "mercenario",
+    militar: "militar",
+    operario: "operario",
+    policial: "policial",
+    religioso: "religioso",
+    servidor_publico: "servidor_publico",
+    teorico_da_conspiracao: "teorico_da_conspiracao",
+    ti: "ti",
+    trabalhador_rural: "trabalhador_rural",
+    trambiqueiro: "trambiqueiro",
+    universitario: "universitario",
+    vitima: "vitima"
+  }
 end
