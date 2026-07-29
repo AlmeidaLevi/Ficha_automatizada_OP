b03480f18546715abd82571df843642ceecde054
Almeida.Levi <almeidalevi878@gmail.com>
2026-07-29 18:46:05 -0300

    Create a test to verify the authentication requirement for protected endpoints

---
+++ test/integration/authentication_required_test.rb
@@ -0,0 +1,142 @@
+require "test_helper"
+
+class AuthenticationRequiredTest < ActionDispatch::IntegrationTest
+  PROTECTED_ENDPOINTS = [
+    {
+      signature: "GET characters#index",
+      verb: :get,
+      path: ->(test) { test.characters_path }
+    },
+    {
+      signature: "GET characters#new",
+      verb: :get,
+      path: ->(test) { test.new_character_path }
+    },
+    {
+      signature: "GET characters#edit",
+      verb: :get,
+      path: ->(test) { test.edit_character_path(test.characters(:one)) },
+      params: ->(test) {
+        {
+          id: test.characters(:one).id
+        }
+      }
+    },
+    {
+      signature: "POST characters#create",
+      verb: :post,
+      path: ->(test) { test.characters_path },
+      params: ->(test) {
+        {
+          character: {
+            name: "Test",
+            origin: "academico",
+            character_class: "combatente",
+            archetype: "aniquilador",
+            current_pv: 0,
+            current_pe: 0,
+            current_sanity: 0,
+            nex: 0,
+            patent: "recruta",
+            prestige_points: 0,
+            element_affiniry: "conhecimento",
+            user_id: test.characters(:one).user_id,
+            for: 1,
+            agi: 1,
+            int: 1,
+            pre: 1,
+            vig: 1
+          }
+        }
+      }
+    },
+    {
+      signature: "PATCH characters#update",
+      verb: :patch,
+      path: ->(test) { test.character_path(test.characters(:one)) },
+      params: ->(test) {
+        {
+          character: {
+            name: "Test",
+            origin: "academico",
+            character_class: "combatente",
+            archetype: "aniquilador",
+            current_pv: 0,
+            current_pe: 0,
+            current_sanity: 0,
+            nex: 0,
+            patent: "recruta",
+            prestige_points: 0,
+            element_affiniry: "conhecimento",
+            user_id: test.characters(:one).user_id,
+            for: 1,
+            agi: 1,
+            int: 1,
+            pre: 1,
+            vig: 1
+          }
+        }
+      }
+    },
+
+    {
+      signature: "PUT characters#update",
+      verb: :put,
+      path: ->(test) { test.character_path(test.characters(:one)) },
+      params: ->(test) {
+        {
+          character: {
+            name: "Test",
+            origin: "academico",
+            character_class: "combatente",
+            archetype: "aniquilador",
+            current_pv: 0,
+            current_pe: 0,
+            current_sanity: 0,
+            nex: 0,
+            patent: "recruta",
+            prestige_points: 0,
+            element_affiniry: "conhecimento",
+            user_id: test.characters(:one).user_id,
+            for: 1,
+            agi: 1,
+            int: 1,
+            pre: 1,
+            vig: 1
+          }
+        }
+      }
+    },
+    {
+      signature: "DELETE characters#edit",
+      verb: :delete,
+      path: ->(test) { test.character_path(test.characters(:one)) },
+      params: ->(test) {
+        {
+          id: test.characters(:one).id
+        }
+      }
+    }
+  ]
+
+  PROTECTED_ENDPOINTS.each do |endpoint|
+    test "#{endpoint.fetch(:signature)} exige autenticação" do
+      path = endpoint.fetch(:path).call(self)
+
+      params =
+      if endpoint[:params]
+        endpoint.fetch(:params).call(self)
+      else
+        {}
+      end
+
+      public_send(
+        endpoint.fetch(:verb),
+        path,
+        params: params
+      )
+
+      assert_redirected_to new_session_path
+    end
+  end
+end
