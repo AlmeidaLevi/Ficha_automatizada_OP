6ed1e0889e37ed3754eef94ffc5b8a55cb12c86e
Almeida.Levi <almeidalevi878@gmail.com>
2026-07-29 19:37:59 -0300

    remove unused session_edit endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :characters, except: :show
-  resource :session, except: [ :show, :update ]
+  resource :session, except: [ :show, :update, :edit ]
   resources :passwords, param: :token
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
