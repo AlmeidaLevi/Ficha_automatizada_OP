19b77a6319d596905a7ccaa9a6fe8800a24d39cf
Almeida.Levi <almeidalevi878@gmail.com>
2026-07-31 19:15:31 -0300

    add new endpoints in PROTECTED_ENDPOINTS constant and fix older endpoints info

--- test/integration/authentication_required_test.rb
+++ test/integration/authentication_required_test.rb
@@ -92,19 +92,54 @@ class AuthenticationRequiredTest < ActionDispatch::IntegrationTest
       }
     },
     {
-      signature: "DELETE characters#edit",
+      signature: "DELETE characters#destroy",
       verb: :delete,
-      path: ->(test) { test.character_path(test.characters(:one)) },
+      path: ->(test) { test.character_path(test.characters(:one)) }
+    },
+    {
+      signature: "DELETE sessions#destroy",
+      verb: :delete,
+      path: ->(test) { test.session_path }
+    },
+    {
+      signature: "GET users#index",
+      verb: :get,
+      path: ->(test) { test.users_path }
+    },
+    {
+      signature: "GET users#edit",
+      verb: :get,
+      path: ->(test) { test.edit_user_path(test.users(:one)) }
+    },
+    {
+      signature: "GET users#show",
+      verb: :get,
+      path: ->(test) { test.user_path(test.users(:one)) }
+    },
+    {
+      signature: "PATCH users#update",
+      verb: :patch,
+      path: ->(test) { test.user_path(test.users(:one)) },
       params: ->(test) {
         {
-          id: test.characters(:one).id
+          user: user_fields
         }
       }
     },
     {
-      signature: "DELETE session#destroy",
+      signature: "PUT users#update",
+      verb: :put,
+      path: ->(test) { test.user_path(test.users(:one)) },
+      params: ->(test) {
+        {
+          user: user_fields
+        }
+      }
+    },
+    {
+      signature: "DELETE users#destroy",
       verb: :delete,
-      path: ->(test) { test.session_path }
+      path: ->(test) { test.user_path(test.users(:one)) }
     }
   ]
 
