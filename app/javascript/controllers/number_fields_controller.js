ba36910211c3dc5a7d01fbfdd221ac124ab07c0e
Almeida.Levi <almeidalevi878@gmail.com>
2026-08-02 21:31:21 -0300

    Implement updateMax, increment, and decrement methods for the PE field in the JS controller

--- app/javascript/controllers/number_fields_controller.js
+++ app/javascript/controllers/number_fields_controller.js
@@ -1,34 +1,40 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = ["PV", "FOR", "AGI", "INT", "PRE", "VIG", "DisplayPV", "NEX"]
+    static targets = ["FOR", "AGI", "INT", "PRE", "VIG", "NEX", "PV", "DisplayPV","PE","DisplayPE"]
     static values = {
-        currentPv: Number
+        currentPv: Number,
+        currentPe: Number
     }
 
     connect() {
         this.maxPV = 0
         this.currentPV = this.currentPvValue
         this.PVTarget.value = this.currentPV
+
+        this.maxPE = 0
+        this.currentPE = this.currentPeValue
+        this.PETarget.value = this.currentPE
+
         this.class_selector = document.getElementById("character_character_class")
-        this.current_class = this.class_selector.value
         this.updateMaxPV()
+        this.updateMaxPE()
     }
 
     updateMaxPV(){
-        this.current_class = this.class_selector.value
+        let current_class = this.class_selector.value
         let vigor = Number(this.VIGTarget.value)
         let nex = Number(this.NEXTarget.value)
         if (nex == 99){
             nex += 1
         }
-        if (this.current_class == "combatente"){
+        if (current_class == "combatente"){
             this.maxPV = 20 + vigor
             this.maxPV += Math.floor(nex/5) * (4 + vigor)
-        }else if (this.current_class == "especialista"){
+        }else if (current_class == "especialista"){
             this.maxPV = 16 + vigor
             this.maxPV += Math.floor(nex/5) * (3 + vigor)
-        }else if (this.current_class == "ocultista"){
+        }else if (current_class == "ocultista"){
             this.maxPV = 12 + vigor
             this.maxPV += Math.floor(nex/5) * (2 + vigor)
         }
@@ -52,6 +58,46 @@ export default class extends Controller {
             this.currentPV -= 1
             this.PVTarget.value = this.currentPV
             this.DisplayPVTarget.textContent = `${this.currentPV}/${this.maxPV}`
+        }
+    }
+
+    updateMaxPE(){
+        let current_class = this.class_selector.value
+        let presenca = Number(this.PRETarget.value)
+        let nex = Number(this.NEXTarget.value)
+        if(nex == 99){
+            nex += 1
+        }
+        if(current_class == "combatente"){
+            this.maxPE = 2 + presenca
+            this.maxPE += Math.floor(nex/5) * (2 + presenca)
+        }else if(current_class == "especialista"){
+            this.maxPE = 3 + presenca
+            this.maxPE += Math.floor(nex/5) * (3 + presenca)
+        }else if(current_class == "ocultista"){
+            this.maxPE = 4 + presenca
+            this.maxPE += Math.floor(nex/5) * (4 + presenca)
+        }
+        if(this.currentPE > this.maxPE){
+            this.currentPE = this.currentPE
+            this.PETarget.value = this.currentPV
+        }
+        this.DisplayPETarget.textContent = `${this.currentPE}/${this.maxPE}`
+    }
+
+    incrementPE(){
+        if(this.currentPE < this.maxPE){
+            this.currentPE += 1
+            this.PETarget.value = this.currentPE
+            this.DisplayPETarget.textContent = `${this.currentPE}/${this.maxPE}`
+        }
+    }
+
+    decrementPE(){
+        if(this.currentPE > 0){
+            this.currentPE -= 1
+            this.PETarget.value = this.currentPE
+            this.DisplayPETarget.textContent = `${this.currentPE}/${this.maxPE}`
         }
     }
 }
